79898e6feca26ecc836fa7e04d846e0b11bb62e8
Patrick Cullen <cullepm3@hotmail.com>
2015-12-15 16:38:40 -0800

    solution

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,97 @@
 
 class Cuboid
-  
-  #BEGIN public methods that should be your starting point
+  attr_accessor :origin, :width, :height, :length
+  X=0
+  Y=1
+  Z=2
+
+  def initialize(x,y,z,w,h,l)
+    if w <=0 || h<=0 || l <=0
+      raise raise ArgumentError.new("length, height and width must be larger than 0")
+    end
+
+    @origin = [x,y,z]
+    @width = w
+    @height = h
+    @length = l
+  end
 
   def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+    @origin = [x,y,z]
   end
 
-  #END public methods that should be your starting point  
+  def move!(axis, distance)
+    case axis
+      when :x
+        move_to!(origin[X]+distance, origin[Y], origin[Z])
+      when :y
+        move_to!(origin[X], origin[Y]+distance, origin[Z])
+      when :z
+        move_to!(origin[X], origin[Y], origin[Z]+distance)
+    end
+  end
+
+  def max_vert
+    [origin[X]+width, origin[Y]+height, origin[Z]+length]
+  end
+
+  # returns true if the two cuboids intersect each other.  False otherwise.
+  def intersects?(other)
+    return false if self.equal?(other)
+    !(above?(other) || below?(other) || right_of?(other) || left_of?(other) || behind?(other) || in_front_of?(other))
+  end
+
+  # rotate 90 degress around center (note this isn't the origin)
+  def rotate!(axis)
+    center = [origin[X]+width/2, origin[Y]+height/2, origin[Z]+length/2]
+    case axis
+    when :x
+      self.height, self.length = self.length, self.height
+    when :y
+      self.width, self.length = self.length, self.width
+    when :z
+      self.width, self.height = self.height, self.width
+    end
+    self.origin = [center[X]-width/2, center[Y]-height/2, center[Z]-length/2]
+  end
+
+  def above?(other)
+    origin[Y] > other.max_vert[Y]
+  end
+
+  def below?(other)
+    max_vert[Y] < other.origin[Y]
+  end
+
+  def right_of?(other)
+    origin[X] > other.max_vert[X]
+  end
+
+  def left_of?(other)
+    max_vert[X] < other.origin[X]
+  end
+
+  def behind?(other)
+    origin[Z] > other.max_vert[Z]
+  end
+
+  def in_front_of?(other)
+    max_vert[Z] < other.origin[Z]
+  end
+
+  def vertices
+    [ origin,
+     [origin[X],       origin[Y],        origin[Z]+length],
+     [origin[X],       origin[Y]+height, origin[Z]],
+     [origin[X],       origin[Y]+height, origin[Z]+length],
+     [origin[X]+width, origin[Y],        origin[Z]],
+     [origin[X]+width, origin[Y],        origin[Z]+length],
+     [origin[X]+width, origin[Y]+height, origin[Z]],
+     max_vert]
+  end
+
+  def to_s
+    "(#{origin[X]}, #{origin[Y]}, #{origin[Z]}), (#{max_vert[X]}, #{max_vert[Y]}, #{max_vert[Z]})"
+  end
+
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,110 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
-#  Do whatever you need to do to make it work and please add your own test cases for as many
-#  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+
+  let (:subject) { Cuboid.new(1,1,1,10,10,10) }
+  let (:other) { Cuboid.new(1,1,1,10,10,10) }
+  let (:rotator) { Cuboid.new(-2,-4,-8,4,8,16) }
+
+  describe "#initialize" do
+    it "raises ArgumentError if width, height, length invalid" do
+      expect{Cuboid.new(1,1,1,-1,0,0)}.to raise_error(ArgumentError)
     end
-  end    
-  
-  describe "intersects?" do
   end
 
+  describe "move!_to" do
+    it "changes the origin in the simple happy case" do
+      expect(subject.move_to!(1,2,3)).to eq [1,2,3]
+    end
+  end
+
+  describe "max_vert" do
+    it "returns the vertex furthest from the origin" do
+      expect(subject.max_vert).to eq [11,11,11]
+    end
+  end
+
+  describe "vertices" do
+    it "returns 8 vertices" do
+      expect(subject.vertices.size).to be 8
+    end
+    it "returns the correct vertices" do
+      o = Cuboid.new(0,0,0,1,2,3)
+      expect(o.vertices[0]).to eq [0,0,0]
+      expect(o.vertices[1]).to eq [0,0,3]
+      expect(o.vertices[2]).to eq [0,2,0]
+      expect(o.vertices[3]).to eq [0,2,3]
+      expect(o.vertices[4]).to eq [1,0,0]
+      expect(o.vertices[5]).to eq [1,0,3]
+      expect(o.vertices[6]).to eq [1,2,0]
+      expect(o.vertices[7]).to eq [1,2,3]
+    end
+  end
+
+  describe "intersects?" do
+    it "returns false when compared to itself" do
+      expect(subject.intersects?(subject)).to be false
+    end
+
+    it "returns true when other is exact same size and position" do
+      expect(subject.intersects?(other)).to be true
+    end
+
+    it "returns true when other is inside" do
+      expect(subject.intersects?(Cuboid.new(2,2,2,1,1,1))).to be true
+    end
+
+    it "returns false when below" do
+      subject.move!(:y, -11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns false when above" do
+      subject.move!(:y, 11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns false when left of" do
+      subject.move!(:x, -11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns false when right of" do
+      subject.move!(:x, 11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns false when in front of" do
+      subject.move!(:z, -11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns false when behind" do
+      subject.move!(:z, 11)
+      expect(subject.intersects?(other)).to be false
+    end
+
+    it "returns true when corners overlap" do
+      subject.move!(:x, 11)
+      expect(subject.intersects?(other)).to be false
+      subject.move!(:x, -1)
+      expect(subject.intersects?(other)).to be true
+    end
+  end
+
+  describe "#rotate" do
+    it "should rotate 90 degrees around x" do
+      rotator.rotate!(:x)
+      expect(rotator.origin).to eq [-2,-8,-4]
+    end
+
+    it "should rotate 90 degrees around x" do
+      rotator.rotate!(:y)
+      expect(rotator.origin).to eq [-8,-4,-2]
+    end
+    it "should rotate 90 degrees around x" do
+      rotator.rotate!(:z)
+      expect(rotator.origin).to eq [-4,-2,-8]
+    end
+  end
 end
